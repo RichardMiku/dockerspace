ffb231bd687e3e0bf1edb964870f5de39aa34058
Richard Miku <richardmiku@vip.qq.com>
2024-07-24 17:03:30 +0800

    frp

--- frp/entrypoint.sh
+++ frp/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 启动frps服务
-screen -S frps -dm /usr/local/bin/frps -c /etc/frp/frps.ini
+screen -S frps -dm /usr/local/bin/frps
 
 # 启动frpc服务
-screen -S frpc -dm /usr/local/bin/frpc -c /etc/frp/frpc.ini
+screen -S frpc -dm /usr/local/bin/frpc
 
 # 保持容器运行
 tail -f /dev/null
