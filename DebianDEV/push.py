ba0dd8d57036a16362ea71c30c841232ec57c3dd
Richard Miku <richardmiku@vip.qq.com>
2024-07-28 00:09:38 +0800

    push

--- DebianDEV/push.py
+++ DebianDEV/push.py
@@ -1,21 +1,27 @@
 #coding=utf-8
 
+import os
 import re
 import json
 import time
-import requests
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import requests
 
 def getCookie():
-    option = webdriver.ChromeOptions()
-    option.add_experimental_option("detach", True)
+    edge_options = EdgeOptions()
+    edge_options.add_experimental_option("detach", True)
+
+    # 设置 EdgeDriver 的路径为当前目录下的 msedgedriver.exe
+    service = EdgeService(os.path.join(os.getcwd(), 'msedgedriver.exe'))
 
     url = "https://sso.douyin.com/?service=https://webcast.amemv.com"
 
-    browser = webdriver.Chrome(options=option)
+    browser = webdriver.Edge(service=service, options=edge_options)
     browser.get(url)
 
     try:
@@ -24,19 +30,21 @@ def getCookie():
             #EC.url_contains('https://webcast.amemv.com')
         )
     except TimeoutException:
-        print("close")
+        print("Timed out waiting for page to load")
 
-    # 登录完成后获取Cookie
+    # 登录完成后获取 Cookie
     cookies = browser.get_cookies()
 
-    # 格式化Cookie
-    cookie_package={}
+    # 格式化 Cookie
+    cookie_package = {}
     for cookie in cookies:
         cookie_package[cookie['name']] = cookie['value']
+
     # 关闭浏览器
     browser.quit()
     print(cookie_package)
     return cookie_package
+
 
 def getRTMPurl(cookie_package):
     url = "https://webcast.amemv.com/webcast/room/get_latest_room/?ac=wifi&app_name=webcast_mate&version_code=2.2.9&device_platform=windows&webcast_sdk_version=1520&resolution=1920%2A1080&os_version=10.0.19043&language=zh&aid=2079&live_id=1&channel=online&device_id=3096676051989080&iid=1117559680415880"
